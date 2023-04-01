fcc4068cce2e67ef4635f05b99ef219e75f059d1
Sami-20s <sma.webdev.frontend.03@gmail.com>
2023-04-01 23:35:48 +0300

    add-layout

--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { LayoutRoutingModule } from './layout-routing.module';
 import { LayoutComponent } from './layout.component';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [LayoutComponent, NavbarComponent],
-  imports: [LayoutRoutingModule, CommonModule],
+  imports: [LayoutRoutingModule, CommonModule, SharedModule],
 })
 export class LayoutModule {}
