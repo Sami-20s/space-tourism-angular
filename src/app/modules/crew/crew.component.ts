6859b371b6f275d741fdbbb77b37e4483daa64dd
Sami-20s <sma.webdev.frontend.03@gmail.com>
2023-04-07 00:15:57 +0300

    add slider to crew page

--- src/app/modules/crew/crew.component.ts
+++ src/app/modules/crew/crew.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CrewDetailsComponent } from './components/crew-details/crew-details.component';
 import { CrewService } from './services/crew.service';
+import { Subscription, interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-crew',
@@ -10,9 +11,30 @@ import { CrewService } from './services/crew.service';
   templateUrl: './crew.component.html',
   styleUrls: ['./crew.component.scss'],
 })
-export class CrewComponent {
-  constructor(private crewService: CrewService) {}
+export class CrewComponent implements OnInit, OnDestroy {
+  currentSlide: number = 0;
+  subscription: Subscription;
+  constructor(private crewService: CrewService) {
+    this.autoSwipe();
+  }
+  ngOnInit(): void {}
+
   getCrew() {
     return this.crewService.crew;
+  }
+  onSlide(dot) {
+    this.currentSlide = dot;
+  }
+  autoSwipe() {
+    this.subscription = interval(10000).subscribe((number) => {
+      if (this.currentSlide == 3) {
+        this.currentSlide = 0;
+      } else {
+        this.currentSlide++;
+      }
+    });
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
