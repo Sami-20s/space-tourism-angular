fed038aedaab427dc9441186465fe02fab397ca9
Sami-20s <sma.webdev.frontend.03@gmail.com>
2023-04-08 01:00:09 +0300

    add loader

--- src/app/modules/crew/crew.component.ts
+++ src/app/modules/crew/crew.component.ts
@@ -2,7 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CrewDetailsComponent } from './components/crew-details/crew-details.component';
 import { CrewService } from './services/crew.service';
-import { Subscription, interval, map } from 'rxjs';
+import { BehaviorSubject, Subscription, interval, map, switchMap } from 'rxjs';
 import { Crew } from './interfaces/crew';
 
 @Component({
@@ -15,6 +15,8 @@ import { Crew } from './interfaces/crew';
 export class CrewComponent implements OnInit, OnDestroy {
   currentSlide: number = 0;
   subscription: Subscription;
+  swiperInterval$ = new BehaviorSubject<any>(null);
+
   constructor(private crewService: CrewService) {
     this.autoSwipe();
   }
@@ -25,15 +27,18 @@ export class CrewComponent implements OnInit, OnDestroy {
   }
   onSlide(dot) {
     this.currentSlide = dot;
+    this.swiperInterval$.next(null);
   }
   autoSwipe() {
-    this.subscription = interval(10000).subscribe((number) => {
-      if (this.currentSlide == 3) {
-        this.currentSlide = 0;
-      } else {
-        this.currentSlide++;
-      }
-    });
+    this.subscription = this.swiperInterval$
+      .pipe(switchMap(() => interval(10000)))
+      .subscribe((number) => {
+        if (this.currentSlide == 3) {
+          this.currentSlide = 0;
+        } else {
+          this.currentSlide++;
+        }
+      });
   }
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
--- src/app/modules/technology/technology.component.ts
+++ src/app/modules/technology/technology.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TechnologyService } from './services/technology.service';
 import { Technology } from './interfaces/technology';
 import { TechnologyDetailsComponent } from './components/technology-details/technology-details.component';
+import { BehaviorSubject, Subscription, interval, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-technology',
@@ -11,14 +12,33 @@ import { TechnologyDetailsComponent } from './components/technology-details/tech
   templateUrl: './technology.component.html',
   styleUrls: ['./technology.component.scss'],
 })
-export class TechnologyComponent implements OnInit {
-  selectedTechno: number = 0;
+export class TechnologyComponent implements OnInit, OnDestroy {
+  currentTechno: number = 0;
+  subscription: Subscription;
+  swiperInterval$ = new BehaviorSubject<any>(null);
   constructor(private technologyService: TechnologyService) {}
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.autoSwipe();
+  }
   selectTechno(techno) {
-    this.selectedTechno = techno;
+    this.currentTechno = techno;
+    this.swiperInterval$.next(null);
   }
   get getTechnologies(): Technology[] {
     return this.technologyService.technologies;
+  }
+  autoSwipe() {
+    this.subscription = this.swiperInterval$
+      .pipe(switchMap(() => interval(10000)))
+      .subscribe((number) => {
+        if (this.currentTechno == 2) {
+          this.currentTechno = 0;
+        } else {
+          this.currentTechno++;
+        }
+      });
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
